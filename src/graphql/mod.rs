6afa5b98c39c700c2681ebf7bfd4956258593587
Abdul Haris Djafar <abdul.djafar@quipper.com>
2023-07-15 17:43:08 +0700

    get id key datas

--- src/graphql/mod.rs
+++ src/graphql/mod.rs
@@ -1,5 +1,9 @@
+
 use serde::Deserialize;
 use serde::Serialize;
+use serde_with::serde_as;
+use serde_json::json;
+
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -10,12 +14,14 @@ pub struct BswpStakehouses {
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Data {
-    pub stakehouse_accounts: Vec<StakehouseAccount>,
+    pub stakehouse_accounts: Vec<StakehouseAccounts>,
 }
 
+
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde_as]
 #[serde(rename_all = "camelCase")]
-pub struct StakehouseAccount {
+pub struct StakehouseAccounts {
     pub id: String,
     pub number_of_deposits: String,
     pub register_validator_block_number: String,
@@ -33,11 +39,13 @@ pub struct StakehouseAccount {
     pub total_slotformatted: String,
     #[serde(rename = "totalCollateralizedSLOTInVaultFormatted")]
     pub total_collateralized_slotin_vault_formatted: String,
+    #[serde_as(deserialize_as = "DefaultOnError")]
+    #[serde(rename = "stakeHouseMetadata")]
+    #[serde(default)]
     pub stake_house_metadata: StakeHouseMetadata,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
 pub struct StakeHouseMetadata {
     #[serde(rename = "sETHRedemptionRateFormatted")]
     pub s_ethredemption_rate_formatted: String,
@@ -48,48 +56,50 @@ pub struct StakeHouseMetadata {
 }
 
 pub struct Bswp {
-    pub graphql_stackhouses_query : &str,
-    pub api_url: &str,
+    pub graphql_stackhouses_query : String,
+    pub api_url: String,
     pub firts: i32,
     pub skip: i32 
 }
 
 impl Bswp {
 
-    pub fn new(&self) -> Self {
+    pub fn new() -> Self {
         let firts = 1000;
         let skip = 0;
-        let api_url = "https://api.thegraph.com/subgraphs/name/bswap-eng/stakehouse-protocol";
-        let graphql_stackhouses_query = format!(r#"
-        {{
-            stakehouseAccounts(
-              orderBy: id
-              orderDirection: desc
-              first: {}
-              skip: {}
-              where: {{
-                registerValidatorBlockNumber_not: "0" 
-              }}
-            ) {{
-              id
-              numberOfDeposits
-              registerValidatorBlockNumber
-              totalETHForSLOTSentToDepositContract
-              lifecycleStatus
-              accountId
-              totalDETHMinted
-              totalSLOT
-              totalDETHMintedFormatted
-              totalSLOTFormatted
-              totalCollateralizedSLOTInVaultFormatted
-              stakeHouseMetadata {{
-                sETHRedemptionRateFormatted
-                sETHExchangeRateFormatted
-                sETHPayoffRateFormatted
-              }}
-            }}
-          }}
-        "#,firts,skip).as_str();
+        let api_url = "https://api.thegraph.com/subgraphs/name/bswap-eng/stakehouse-protocol".to_string();
+        let graphql_stackhouses_query = format!(
+            r#"{{
+                stakehouseAccounts(
+                    orderBy: id
+                    orderDirection: desc
+                    first: {}
+                    skip: {}
+                    where: {{
+                        registerValidatorBlockNumber_not: "0",
+                        lifecycleStatus_gt: "2",
+                        stakeHouseMetadata_not: "null"
+
+                    }}
+                ) {{
+                    id
+                    numberOfDeposits
+                    registerValidatorBlockNumber
+                    totalETHForSLOTSentToDepositContract
+                    lifecycleStatus
+                    accountId
+                    totalDETHMinted
+                    totalSLOT
+                    totalDETHMintedFormatted
+                    totalSLOTFormatted
+                    totalCollateralizedSLOTInVaultFormatted
+                    stakeHouseMetadata {{
+                        sETHRedemptionRateFormatted
+                        sETHExchangeRateFormatted
+                        sETHPayoffRateFormatted
+                    }}
+                }}
+            }}"#,firts,skip);
 
         return Bswp { 
             graphql_stackhouses_query,
@@ -98,20 +108,71 @@ impl Bswp {
             api_url
         }
     }
-    pub fn get_public_key_datas(&self) -> Result<BswpStakehouses, reqwest::Error> {
-        let url = self.api_url;
+
+
+    pub  fn set_firts(&mut self, value: i32) {
+        self.firts = value;
+    }
+
+    pub  fn set_skip(&mut self, value: i32) {
+        self.skip = value;
+    }
+
+    pub fn update_graphql_query(&mut self)  {
+        let firts = self.firts;
+        let skip = self.skip;
+
+        let graphql_stackhouses_query = format!(
+            r#"{{
+                stakehouseAccounts(
+                    orderBy: id
+                    orderDirection: desc
+                    first: {}
+                    skip: {}
+                    where: {{
+                        registerValidatorBlockNumber_not: "0",
+                        lifecycleStatus_gt: "2",
+                        stakeHouseMetadata_not: "null"
+                    }}
+                ) {{
+                    id
+                    numberOfDeposits
+                    registerValidatorBlockNumber
+                    totalETHForSLOTSentToDepositContract
+                    lifecycleStatus
+                    accountId
+                    totalDETHMinted
+                    totalSLOT
+                    totalDETHMintedFormatted
+                    totalSLOTFormatted
+                    totalCollateralizedSLOTInVaultFormatted
+                    stakeHouseMetadata {{
+                        sETHRedemptionRateFormatted
+                        sETHExchangeRateFormatted
+                        sETHPayoffRateFormatted
+                    }}
+                }}
+            }}"#,firts,skip);
+
+            self.graphql_stackhouses_query = graphql_stackhouses_query;
+    }
+
+    pub async fn get_public_key_datas(&self) -> Result<BswpStakehouses, reqwest::Error> {
+        let url = self.api_url.to_string();
+        let query = &self.graphql_stackhouses_query;
         
+        let client = reqwest::Client::new();
+        let payload = json!({
+            "query": query,
+        });
         
-        let client = reqwest::blocking::Client::new();
         let response = client
             .post(url)
-            .json(&serde_json::json!({
-                graphql_stackhouses_query
-            }))
-            .send()?;
+            .json(&payload)
+            .send().await?;
         
-        let data: BswpStakehouses = response.json()?;
-        Ok(data)
+            let data: BswpStakehouses = response.json().await?;
+            Ok(data)
     }
     
     
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,34 @@
-use bswp_pipeline::graphql::BswpStakehouses;
+use bswp_pipeline::graphql::{Bswp, BswpStakehouses};
+use tokio::fs::File;
+use tokio::io::AsyncWriteExt;
 
-fn main() {
-    let bswp_stakehouses = BswpStakehouses::new();
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut bswp_stakehouses = Bswp::new();
+    let mut page = 0;
+    bswp_stakehouses.set_firts(1000);
+
+    loop {
+        bswp_stakehouses.set_skip(page);
+        bswp_stakehouses.update_graphql_query();
+
+        let datas: BswpStakehouses = bswp_stakehouses.get_public_key_datas().await?;
+        if datas.data.stakehouse_accounts.is_empty() {
+            println!("no datas");
+            break;
+        }
+
+        let file_name = format!("data/stack_houses_page_{}.json", page);
+        let mut file = File::create(&file_name).await?;
+        
+        for data in datas.data.stakehouse_accounts {
+            let data_str = serde_json::to_string(&data)?;
+            file.write_all(data_str.as_bytes()).await?;
+            file.write_all("\n".as_bytes()).await?;
+        }
+
+        page += 1;
+    }
+
+    Ok(())
 }
